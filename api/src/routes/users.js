2f403f3306e11b0f7119557db954c9ad8248571c
Andres Castillo <pettrovich@gmail.com>
2022-06-22 16:37:59 -0500

    controllers de users agregados

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -1,4 +1,18 @@
 const {Router} = require('express');
+const {getAdminUsers,getNonAdminUsers,getUserByUsername} = require("../controllers/getUsers");
 const router = Router();
+
+router.get('/', async (req,res) => {
+    return await getNonAdminUsers(res);
+});
+
+router.get('/admin', async (req,res) => {
+    return await getAdminUsers(res);
+});
+
+router.get('/:username', async (req,res) => {
+    const {username} = req.params;
+    return await getUserByUsername(username,res);
+});
 
 module.exports = router;
