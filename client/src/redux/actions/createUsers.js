7ac78dc3a0efaa1d7736fbae804564410a582622
LautaroZalazar <lautyzalazar10@gmail.com>
2022-06-24 17:18:24 -0300

    Actions sin localHost por configuración y formik en package.json

--- client/src/redux/actions/createUsers.js
+++ client/src/redux/actions/createUsers.js
@@ -3,6 +3,6 @@ export const ADD_USER = 'ADD_USER';
 
 export const addUserDb = (data) => {
     return async function() {
-        return await axios.post('http://localhost:3001/user', data)
+        return await axios.post('/user', data)
     }
 }
