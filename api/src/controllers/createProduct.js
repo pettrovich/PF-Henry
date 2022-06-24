3817a870825f84a283908a5b085f9b37998aa3c4
Martin <martinsebastian1553@outlook.com>
2022-06-24 19:34:07 -0300

    (api(*): added free shipping GET route)

--- api/src/controllers/createProduct.js
+++ api/src/controllers/createProduct.js
@@ -1,7 +1,7 @@
 const {Product} = require("../db")
 
 const createProduct = async (req, res) => {
-    const {name, image, price, description, categories, stock} = req.body
+    const {name, image, price, description, categories, stock, freeShipping, brand} = req.body
     try {
         await Product.create({
             name,
@@ -9,11 +9,13 @@ const createProduct = async (req, res) => {
             price,
             description,
             categories,
-            stock
+            stock,
+            freeShipping,
+            brand
         })
         res.send(`Producto ${name} creado exitosamente`)
     } catch (error) {
-        res.send("No se pudo crear el producto")
+        res.status(404).send("No se pudo crear el producto")
     }
 }
 
