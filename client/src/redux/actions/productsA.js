bb2ee0ef431a40fe52b21208838e551f031232c5
cirkuit32 <emilianorottaf@gmail.com>
2022-06-25 18:25:45 -0300

    filtrados y mejoras en el formulario

--- client/src/redux/actions/productsA.js
+++ client/src/redux/actions/productsA.js
@@ -4,6 +4,11 @@ export const GET_ALL_PRODUCTS = 'GET_ALL_PRODUCTS';
 export const ORDER_PRICE = 'ORDER_PRICE';
 export const FILTER_CATEGORY = 'FILTER_CATEGORY';
 
+export const RANGO_PRICE = 'RANGO_PRICE';
+export const DISCOUNT = 'DISCOUNT';
+export const ENVIOS = 'ENVIOS';
+export const GET_BRAND = 'GET_BRAND';
+
 export const getAllProducts = () => {
     return async function (dispatch) {
         const response = (await axios.get('/catalog')).data;
@@ -24,4 +29,51 @@ export const getByCategory = (data) => {
         const response = (await axios.get(`/filter/${data}`)).data;
         return dispatch({ type: FILTER_CATEGORY, payload: response })
     }
+}
+export function rangoByPrice (payload){
+    return async function (dispatch) {
+      var json = await axios.get (`http://localhost:3001/filterPriceRange/${payload}`);
+            return dispatch ({
+                type: RANGO_PRICE,
+                payload: json.data
+            })
+    }
+}
+
+
+export function byDiscount (payload){
+    return async function (dispatch) {
+      var json = await axios.get (`http://localhost:3001/VER FILTRO DE DESCUENTO!!!!!!!/${payload}`);
+            return dispatch ({
+                type: DISCOUNT,
+                payload: json.data
+            })
+    }
+}
+
+export function byEnvios (payload){
+    return async function (dispatch) {
+      var json = await axios.get (`http://localhost:3001/filterShipping/${payload}`);
+            return dispatch ({
+                type: ENVIOS,
+                payload: json.data
+            })
+    }
+}
+
+export function getbrand(payload){
+    return async function (dispatch){
+        try{
+            var json = await axios.get (`http://localhost:3001/filterBrand/${payload}`);
+            return dispatch({
+                type: GET_BRAND,
+                payload: json.data
+
+            })
+        }catch (error){
+            
+            alert ("No tenemos esa marca.")
+      
+    }
+}
 }
