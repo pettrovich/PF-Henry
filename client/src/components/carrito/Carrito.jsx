890349db86709ee24d6d38b1d1a748048ce0c993
Lautaro Darretche <lautarodarretche@hotmail.es>
2022-06-29 00:12:18 -0300

    (redux & components) Added localStorage to carrito & favorites

--- client/src/components/carrito/Carrito.jsx
+++ client/src/components/carrito/Carrito.jsx
@@ -9,17 +9,12 @@ import CarritoCards from './CarritoCards';
 function Carrito({ resetTotal }) {
     const products = useSelector((state) => state.carrito);
 
-    // let total
-    // if (products.length <= 0) total = 0
-    // else total = Math.trunc(products.map(e => e.price).reduce((prevValue, currentValue) => prevValue + currentValue)) // Saca el total de los productos dentro del carrito
-
     useEffect(() => {
         return () => {
             resetTotal()
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
-
 
     return (
 
@@ -28,21 +23,21 @@ function Carrito({ resetTotal }) {
                 <div className={style.products}>
                     {
                         (products.productosCarrito.length === 0)
-                            ?   <div> <br/> 
-                                    <h1 className={style.title}>No has agregado productos al carrito... </h1>
-                                    <Link to = "/products" > <button className = {style.verProductos}>Ver productos</button> </Link>
-                                </div> 
+                            ? <div> <br />
+                                <h1 className={style.title}>No has agregado productos al carrito... </h1>
+                                <Link to="/products" > <button className={style.verProductos}>Ver productos</button> </Link>
+                            </div>
                             : products.productosCarrito.map(e =>
-                            (
-                                < CarritoCards
-                                    key={e.id}
-                                    id={e.id}
-                                    name={e.name}
-                                    image={e.image}
-                                    price={e.price}
-                                    stock={e.stock}
-                                />
-                            ))}
+                            (< CarritoCards
+                                key={e.id}
+                                id={e.id}
+                                name={e.name}
+                                image={e.image}
+                                price={e.price}
+                                stock={e.stock}
+                            />
+                            ))
+                    }
                 </div>
                 <div className={style.containerTotal}>
                     <div className={style.cardTotal}>
