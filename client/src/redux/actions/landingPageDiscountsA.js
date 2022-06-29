59faa04d98f31d821bbb62c58d5547802f6deb26
cirkuit32 <emilianorottaf@gmail.com>
2022-06-29 16:57:28 -0300

    LandingPage, cambio de card; ofertas, productos vendidos, nuevos productos

--- client/src/redux/actions/landingPageDiscountsA.js
+++ client/src/redux/actions/landingPageDiscountsA.js
@@ -4,7 +4,7 @@ export const DISCOUNTS = 'DISCOUNTS';
 
 export const  getDiscounts = ()=> {
     return async function (dispatch) {
-        var json = await axios.get('/newestProducts'); //CAMBIAR RUTA POR LA DE DESCUENTO
+        var json = await axios.get('/mostDiscountedProducts'); 
         // console.log ("acciones",json.data)
         return dispatch({
             type: DISCOUNTS,
--- client/src/redux/actions/landingPageMostSelledA.js
+++ client/src/redux/actions/landingPageMostSelledA.js
@@ -4,7 +4,7 @@ export const MOST_SELLED_PRODUCTS = 'MOST_SELLED_PRODUCTS';
 
 export const  getMostSelled = ()=> {
     return async function (dispatch) {
-        var json = await axios.get('/newestProducts'); //PONER LA RUTA DE LOS PRODUCTOS MAS VENDIDOS
+        var json = await axios.get('/newestProducts'); //PONER LA RUTA DE LOS PRODUCTOS MAS VENDIDOS /bestSellingProducts
         // console.log ("acciones",json.data)
         return dispatch({
             type: MOST_SELLED_PRODUCTS,
