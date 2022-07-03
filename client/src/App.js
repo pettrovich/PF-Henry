921b9ff1c13c5e9f2d4705ed51c6914d51e0adce
Lautaro Darretche <lautarodarretche@hotmail.es>
2022-07-02 22:28:08 -0300

    (public/index.html) & (client/App.js) Configuraciones para el carrito

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 import { Route, Routes } from "react-router-dom";
 import Navbar from './components/navbar/Navbar';
 import Products from './components/products/Products';
-import Carrito from './components/carrito/Carrito';
 import LandingPage from './components/landingPage/LandingPage';
 import DetailProduct from './components/detallesDelProducto/DetallesDelProducto';
 import PerfilDelUsuario from "./components/perfilDelUsuario/PerfilDelUsuario";
 import Favoritos from './components/favoritos/Favoritos';
 import Dashboard from './components/dashboard/Dashboard';
 import Material_UI from './components/material_UI/Material_UI';
+import CarritoContainer from './components/carrito/CarritoContainer';
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
       <Routes>
         <Route path="" element={<LandingPage />} />
         <Route path="products" element={<Products />} />
-        <Route path="carrito" element={<Carrito />} />
+        <Route path="carrito" element={<CarritoContainer />} />
         <Route path="/detail/:id" element={<DetailProduct />} />
         <Route path="/profile" element={<PerfilDelUsuario />} />
         <Route path="/favoritos" element={<Favoritos />} />
