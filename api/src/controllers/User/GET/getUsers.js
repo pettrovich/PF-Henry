5ab5e548d274901ab497981380ca71d1c97f03ee
LautaroZalazar <lautyzalazar10@gmail.com>
2022-07-06 20:13:20 -0300

    Bug fixes

--- api/src/controllers/User/GET/getUsers.js
+++
@@ -1,9 +0,0 @@
-const {User} = require('../../../db')
-
-async function getUsers() {
-    const where = {isAdmin: false, banned: false};
-    const userList = await User.findAll({where});
-    return userList;
-}
-
-module.exports = getUsers;
