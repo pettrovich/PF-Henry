48f6cea1f6204663304f0ba88462feb2a8756e70
Andres Castillo <pettrovich@gmail.com>
2022-07-06 12:31:57 -0500

    user routes+

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -1,12 +1,15 @@
-const { Router } = require('express');
+const {Router} = require('express');
+const createUser = require("../controllers/User/POST/createUser");
 const getAdmins = require("../controllers/User/GET/getAdmins");
 const getBannedUsers = require("../controllers/User/GET/getBannedUsers");
 const getUserByEmail = require("../controllers/User/GET/getUserByEmail");
 const getUserById = require("../controllers/User/GET/getUserById");
 const getUsers = require("../controllers/User/GET/getUsers");
+const updateUserInfo = require("../controllers/User/PUT/updateUserInfo");
+const switchAdmin = require("../controllers/User/PUT/switchAdmin");
+const switchBan = require("../controllers/User/PUT/switchBan");
 // const { addFavourite, getFavourites, removeFavourite } = require("../controllers/Product/GET/favourites");
 // const { createAddress, getAddress, updateAddress } = require("../controllers/Address/GET/addresses");
-// const { createUser, updateUserInfo } = require("../controllers/User/GET/users");
 const router = Router();
 
 router.get('/', async (req, res) => {
@@ -50,8 +53,6 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-
-
 router.post('/', async (req, res) => {
     const {name, email} = req.body;
     try {
@@ -63,11 +64,9 @@ router.post('/', async (req, res) => {
     catch (err) {
         return res.status(500).send(`No se pudo crear el usuario (${err})`);
     }
-    // const { address, number, zipCode, country, houseType } = req.body;
-    // const userAddress = await createAddress(address, number, zipCode, province, location, apartment, description);
 });
 
-router.put('/userInfo/:id', async (req, res) => {
+router.put('/info/:id', async (req, res) => {
     const {id} = req.params;
     const userData = req.body;
     try {
@@ -79,90 +78,101 @@ router.put('/userInfo/:id', async (req, res) => {
     }
 });
 
+router.put('/switchAdmin/:id', async (req, res) => {
+    const {id} = req.params;
+    try {
+        const user = await switchAdmin(id);
+        return res.json(user);
+    }
+    catch (err) {
+        return res.status(500).send(`No se pudo modificar el usuario (${err})`);
+    }
+});
+
+router.put('/switchBan/:id', async (req, res) => {
+    const {id} = req.params;
+    try {
+        const user = await switchBan(id);
+        return res.json(user);
+    }
+    catch (err) {
+        return res.status(500).send(`No se pudo modificar el usuario (${err})`);
+    }
+});
+
 //******************* */
 
-router.get('/:username/address', async (req, res) => {
-    const { username } = req.params;
-    try {
-        const address = await getAddress(username);
-        return res.json(address);
-    }
-    catch (err) {
-        return res.status(404).send(`No se pudo cargar la información de la dirección (${err})`);
-    }
-});
+// router.get('/:username/address', async (req, res) => {
+//     const { username } = req.params;
+//     try {
+//         const address = await getAddress(username);
+//         return res.json(address);
+//     }
+//     catch (err) {
+//         return res.status(404).send(`No se pudo cargar la información de la dirección (${err})`);
+//     }
+// });
 
-router.get('/:username/favourites', async (req, res) => {
-    const { username } = req.params;
-    try {
-        const favourites = await getFavourites(username);
-        return res.json(favourites);
-    }
-    catch (err) {
-        return res.status(404).send(`No se pudo cargar la información de productos favoritos (${err})`);
-    }
-});
+// router.get('/:username/favourites', async (req, res) => {
+//     const { username } = req.params;
+//     try {
+//         const favourites = await getFavourites(username);
+//         return res.json(favourites);
+//     }
+//     catch (err) {
+//         return res.status(404).send(`No se pudo cargar la información de productos favoritos (${err})`);
+//     }
+// });
 
-router.post('/:username/addFavourite', async (req, res) => {
-    const { username } = req.params;
-    const { productId } = req.query;
-    try {
-        if (!productId) return res.status(404).send('No se seleccionó ningún producto');
-        const product = await addFavourite(username);
-        return res.json(product);
-    }
-    catch (err) {
-        return res.status(404).send(`No se pudo agregar el producto a favoritos (${err})`);
-    }
-});
+// router.post('/:username/addFavourite', async (req, res) => {
+//     const { username } = req.params;
+//     const { productId } = req.query;
+//     try {
+//         if (!productId) return res.status(404).send('No se seleccionó ningún producto');
+//         const product = await addFavourite(username);
+//         return res.json(product);
+//     }
+//     catch (err) {
+//         return res.status(404).send(`No se pudo agregar el producto a favoritos (${err})`);
+//     }
+// });
 
-router.post('/:username/addFavourite', async (req, res) => {
-    const { username } = req.params;
-    const { productId } = req.query;
-    try {
-        if (!productId) return res.status(404).send('No se seleccionó ningún producto');
-        const favourites = await addFavourite(username);
-        return res.json(favourites);
-    }
-    catch (err) {
-        return res.status(404).send(`No se pudo agregar el producto a favoritos (${err})`);
-    }
-});
+// router.post('/:username/addFavourite', async (req, res) => {
+//     const { username } = req.params;
+//     const { productId } = req.query;
+//     try {
+//         if (!productId) return res.status(404).send('No se seleccionó ningún producto');
+//         const favourites = await addFavourite(username);
+//         return res.json(favourites);
+//     }
+//     catch (err) {
+//         return res.status(404).send(`No se pudo agregar el producto a favoritos (${err})`);
+//     }
+// });
 
-router.post('/:username/removeFavourite', async (req, res) => {
-    const { username } = req.params;
-    const { productId } = req.query;
-    try {
-        if (!productId) return res.status(404).send('No se seleccionó ningún producto');
-        const favourites = await removeFavourite(username);
-        return res.json(favourites);
-    }
-    catch (err) {
-        return res.status(404).send(`No se pudo eliminar el producto de los favoritos (${err})`);
-    }
-});
+// router.post('/:username/removeFavourite', async (req, res) => {
+//     const { username } = req.params;
+//     const { productId } = req.query;
+//     try {
+//         if (!productId) return res.status(404).send('No se seleccionó ningún producto');
+//         const favourites = await removeFavourite(username);
+//         return res.json(favourites);
+//     }
+//     catch (err) {
+//         return res.status(404).send(`No se pudo eliminar el producto de los favoritos (${err})`);
+//     }
+// });
 
-router.put('/:username/address', async (req, res) => {
-    const { username } = req.params;
-    const addressData = req.body;
-    try {
-        const address = await updateAddress(username, addressData);
-        return res.json(address);
-    }
-    catch (err) {
-        return res.status(404).send(`No se pudo modificar la información de la dirección (${err})`);
-    }
-});
-
-router.delete('/:username', async (req, res) => {
-    const { username } = req.params;
-    try {
-        let rows = await deleteUser(username);
-        return res.status(204).json(`${rows} usuario eliminado`);
-    }
-    catch (err) {
-        return res.status(404).send(`No se pudo eliminar el usuario (${err})`);
-    }
-});
+// router.put('/:username/address', async (req, res) => {
+//     const { username } = req.params;
+//     const addressData = req.body;
+//     try {
+//         const address = await updateAddress(username, addressData);
+//         return res.json(address);
+//     }
+//     catch (err) {
+//         return res.status(404).send(`No se pudo modificar la información de la dirección (${err})`);
+//     }
+// });
 
 module.exports = router;
