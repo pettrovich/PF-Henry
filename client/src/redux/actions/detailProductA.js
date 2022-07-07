cd48153ba9716de3deaf3bf3e8d0f895b713e6f6
cirkuit32 <emilianorottaf@gmail.com>
2022-07-07 14:00:49 -0300

    problemas de la Dashboard resueltos

--- client/src/redux/actions/detailProductA.js
+++ client/src/redux/actions/detailProductA.js
@@ -1,10 +1,19 @@
 import axios from "axios";
 
 export const GET_PRODUCT_ID = 'GET_PRODUCT_ID';
+export const REFRESH = 'REFRESH';
 
 export const getOneProduct = (id) => {
     return async function (dispatch) {
         const response = (await axios.get(`/ProductDetail/${id}`)).data;
         return dispatch({ type: GET_PRODUCT_ID, payload: response })
+    }
+}
+
+
+export const refresh = (id) => {
+    return async function (dispatch) {
+        // const response = (await axios.get(`/ProductDetail/${id}`)).data;
+        return dispatch({ type: REFRESH,  })
     }
 }
