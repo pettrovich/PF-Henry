804c8cb1d856c638f25fbf2271eea3e5581d4927
Andres Castillo <pettrovich@gmail.com>
2022-07-07 06:32:48 -0500

    f55

--- api/src/controllers/User/PUT/switchBan.js
+++ api/src/controllers/User/PUT/switchBan.js
@@ -2,7 +2,6 @@ const {User} = require('../../../db');
 
 async function switchBan(id) {
     const where = {id};
-    console.log(id);
     let user = await User.findOne({where});
     if (!user) throw new Error('El usuario no existe en la base de datos.');
     user.set({banned: !user.banned});
