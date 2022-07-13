cb17fb655f3aa3481e95e40fe63ede643b7fd140
LautaroZalazar <lautyzalazar10@gmail.com>
2022-07-13 16:00:19 -0300

    Detalle ordenes de PP

--- client/src/components/perfilDelUsuario/OrderDetailPP.jsx
+++ client/src/components/perfilDelUsuario/OrderDetailPP.jsx
@@ -10,8 +10,8 @@ import Paper from '@mui/material/Paper';
 
 import { Fragment, useEffect, useState } from 'react';
 import {useDispatch, useSelector} from "react-redux"
-import  { userDetail }  from '../../redux/actions/userOrderA';
-import {useLocation} from "react-router-dom";
+import  { userDetailPP }  from '../../redux/actions/userOrderA';
+import {useLocation, useParams} from "react-router-dom";
 
 import Button from '@mui/material/Button';
 import EditIcon from '@mui/icons-material/Edit';
@@ -48,10 +48,10 @@ export default function DashboardUsers() {
     const dispatch = useDispatch()
 
     const location = useLocation()
-    let id = (location.pathname.substring(13,location.pathname.length))
+    let id = (location.pathname.substring(15,location.pathname.length))
 
     useEffect(()=>{
-      dispatch (userDetail(id))
+      dispatch (userDetailPP(id))
     },[dispatch])
 
     const list = useSelector ((state) => state.userOrderR.listOrder);
@@ -66,23 +66,21 @@ export default function DashboardUsers() {
         <TableHead>
           <TableRow>
             <StyledTableCell align="right">Nombre</StyledTableCell>
-            <StyledTableCell align="right">Categoria</StyledTableCell>
             <StyledTableCell align="right">Cantidad</StyledTableCell>
-            <StyledTableCell align="right">Precio unitario($)</StyledTableCell>
+            <StyledTableCell align="right">Precio unitario(U$D)</StyledTableCell>
             <StyledTableCell align="right">Descripción</StyledTableCell>
             <StyledTableCell align="right">Estado de la compra</StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {list.items? list.items.map (i=>
-            <StyledTableRow key={i.title}>
+        {list?.purchase_units && list.purchase_units[0].items? list.purchase_units[0].items.map (i=>
+            <StyledTableRow key={i.name}>
               
-              <StyledTableCell align="right">{i.title}</StyledTableCell>
-              <StyledTableCell align="right">{i.category_id}</StyledTableCell>
+              <StyledTableCell align="right">{i.name}</StyledTableCell>
               <StyledTableCell align="right">{i.quantity}</StyledTableCell>
-              <StyledTableCell align="right">{i.unit_price}</StyledTableCell>
+              <StyledTableCell align="right">{i.unit_amount.value}</StyledTableCell>
               <StyledTableCell align="right">{i.description}</StyledTableCell>
-              <StyledTableCell align="right">{(list.status === "approved" || list.status === "APPROVED")? "Aprobado" : "Rechazado"}</StyledTableCell>
+              <StyledTableCell align="right">{(list.status === "APPROVED" || list.status === "COMPLETED")? "Aprobado" : "Rechazado"}</StyledTableCell>
             </StyledTableRow>
           ) : <Loading/>
         }
@@ -99,5 +97,3 @@ export default function DashboardUsers() {
 
   );
 }
-
-       
